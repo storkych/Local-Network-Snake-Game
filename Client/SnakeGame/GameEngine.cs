791e22dd33eb7d6a9c06893bf9e17f0804e3f045
JulKhizhn <127053070+JulKhizhn@users.noreply.github.com>
2024-12-27 18:02:44 +0300

    Update GameEngine.cs

--- Client/SnakeGame/GameEngine.cs
+++ Client/SnakeGame/GameEngine.cs
@@ -10,36 +10,47 @@ namespace SnakeGame
 {
     internal class GameEngine
     {
-        private const int MAP_WIDTH = 20;
-        private const int MAP_HEIGHT = 15;
+        private const int MAP_WIDTH = 30;
+        private const int MAP_HEIGHT = 20;
+        // Максимальное количество рекордов для отображения.
         private const int MAX_RECORDS = 10;
+        // Ширина экрана, рассчитываемая на основании ширины карты.
         private const int SCREEN_WIDTH = MAP_WIDTH * 3;
+        // Высота экрана, рассчитываемая на основании высоты карты.
         private const int SCREEN_HEIGHT = MAP_HEIGHT * 3;
-        private const int FRAME_MILLISECONDS = 100;
-
+        // Время обновления экрана в миллисекундах.
+        private const int FRAME_MILLISECONDS = 200;
         private const ConsoleColor BORDER_COLOR = ConsoleColor.White;
         private const ConsoleColor FOOD_COLOR = ConsoleColor.Green;
         private const ConsoleColor BODY_COLOR = ConsoleColor.White;
         private const ConsoleColor HEAD_COLOR = ConsoleColor.DarkGray;
-
         private static readonly Random Random = new Random();
-
+        // Текущее состояние игры.
         public GameState gameState;
+        // Данные о состоянии игры.
         private GameStateData gameStateData;
+        // Объект для отображения заголовка.
         private readonly TitlePrinter printer;
+        // Объект для управления сохранением и загрузкой данных.
         private readonly SaveLoadManager saveLoadManager;
-
+        // Запрос паузы в игре.
         static bool pauseRequested = false;
-
-        public Direction opponentDirection = Direction.Right; // Направление по умолчанию
+        // Направление противника по умолчанию.
+        public Direction opponentDirection = Direction.Right;
+        // Флаг готовности второго игрока.
         public bool secondPlayerReady = false;
-
+        // Объект клиента для работы с сервером.
         private GameClient gameClient;
+        // Флаг, указывающий, является ли клиент хостом.
         public bool isClientHost = true;
 
+        /// <summary>
+        /// Конструктор класса GameEngine.
+        /// </summary>
+        /// <param name="client">Клиент для взаимодействия с сервером.</param>
         public GameEngine(GameClient client)
         {
-            gameState = GameState.MainMenu;
+            gameState = GameState.MainMenu; // Инициализация состояния игры.
             gameStateData = new GameStateData();
             printer = new TitlePrinter();
             saveLoadManager = new SaveLoadManager();
@@ -49,17 +60,17 @@ namespace SnakeGame
         }
 
         /// <summary>
-        /// Логика работы игры (главное меню, меню паузы).
+        /// Основной метод логики работы игры (главное меню, меню паузы).
         /// </summary>
         public void Run()
         {
             GameStateData match = new GameStateData();
-
             bool exitRequested = false;
             gameStateData = saveLoadManager.LoadData();
             CursorVisible = false;
             List<string> records = saveLoadManager.ReadRecords();
 
+            // Сортировка рекордов по убыванию
             records.Sort((a, b) => int.Parse(b.Split(' ')[1]) - int.Parse(a.Split(' ')[1]));
 
             ConsoleKeyInfo keyInfo;
@@ -100,6 +111,9 @@ namespace SnakeGame
             }
         }
 
+        /// <summary>
+        /// Отображает информацию о клиенте.
+        /// </summary>
         private void DisplayClientInfo()
         {
             WriteLine($"IS HOST: {isClientHost}");
@@ -108,6 +122,11 @@ namespace SnakeGame
             WriteLine($"SECOND PLAYER: {gameClient.secondPlayer}");
         }
 
+        /// <summary>
+        /// Отображает главное меню игры и обрабатывает выбор пользователя.
+        /// </summary>
+        /// <param name="selectedItem">Выбранный пункт меню (передается по ссылке).</param>
+        /// <param name="exitRequested">Флаг запроса выхода из игры (передается по ссылке).</param>
         private void DisplayMainMenu(ref int selectedItem, ref bool exitRequested)
         {
             string[] menuItems = { "Новая игра", "Таблица рекордов", "Выход" };
@@ -132,15 +151,19 @@ namespace SnakeGame
             {
                 if (selectedItem == menuItems.Length - 1)
                 {
-                    exitRequested = true;
+                    exitRequested = true; // Выход из игры.
                 }
                 else if (selectedItem == 0)
                 {
-                    gameState = GameState.Lobby;
+                    gameState = GameState.Lobby; // Переход в лобби.
                 }
             }
         }
 
+        /// <summary>
+        /// Отображает меню лобби и обрабатывает выбор пользователя.
+        /// </summary>
+        /// <param name="selectedItem">Выбранный пункт меню (передается по ссылке).</param>
         private void ShowLobbyMenu(ref int selectedItem)
         {
             string[] menuItems = { "Запустить игру" };
@@ -165,11 +188,15 @@ namespace SnakeGame
             {
                 if (selectedItem == 0)
                 {
-                    gameClient.StartGame();
+                    gameClient.StartGame(); // Запуск игры.
                 }
             }
         }
 
+        /// <summary>
+        /// Запускает основную игру.
+        /// </summary>
+        /// <returns>Данные о состоянии игры по окончании.</returns>
         private GameStateData StartGame()
         {
             bool isGameOver = false;
@@ -192,14 +219,15 @@ namespace SnakeGame
             var sw = new Stopwatch();
             int lagMs = 0;
 
+            // Игровой цикл.
             while (!isGameOver)
             {
                 HandleUserInput(ref dir1, ref dir2);
 
-                // Обновляем поле игрока 1
+                // Обновляем поле игрока 1.
                 if (snake1.Head.X == food1.X && snake1.Head.Y == food1.Y)
                 {
-                    snake1.Move(dir1, true);
+                    snake1.Move(dir1, true); // Увеличение змеи при поедании еды.
                     food1 = GenFood(snake1);
                     food1.Draw();
                     score1++;
@@ -209,10 +237,10 @@ namespace SnakeGame
                     snake1.Move(dir1);
                 }
 
-                // Обновляем поле игрока 2
+                // Обновляем поле игрока 2.
                 if (snake2.Head.X == food2.X && snake2.Head.Y == food2.Y)
                 {
-                    snake2.Move(dir2, true);
+                    snake2.Move(dir2, true); // Увеличение змеи при поедании еды.
                     food2 = GenFood(snake2);
                     food2.Draw();
                     score2++;
@@ -222,52 +250,63 @@ namespace SnakeGame
                     snake2.Move(dir2);
                 }
 
-                // Проверка на столкновения
+                // Проверка на столкновения.
                 if (CheckCollision(snake1) || CheckCollision(snake2))
                 {
-                    isGameOver = true;
+                    isGameOver = true; // Конец игры.
                 }
 
+                // Отображаем счёт.
                 DisplayScores(score1, score2);
 
-                Thread.Sleep(FRAME_MILLISECONDS - lagMs);
-                sw.Restart();
-                lagMs = (int)sw.ElapsedMilliseconds;
+                Thread.Sleep(FRAME_MILLISECONDS - lagMs); // Задержка между кадрами.
+                sw.Restart(); // Сбрасываем таймер.
+                lagMs = (int)sw.ElapsedMilliseconds; // Задержка.
             }
 
-            gameState = GameState.Lobby;
+            gameState = GameState.Lobby; // Возвращаемся в лобби по окончании игры.
             return new GameStateData();
         }
 
+        /// <summary>
+        /// Обрабатывает ввод пользователя для управления змеями.
+        /// </summary>
+        /// <param name="dir1">Направление первой змеи (передается по ссылке).</param>
+        /// <param name="dir2">Направление второй змеи (передается по ссылке).</param>
         private void HandleUserInput(ref Direction dir1, ref Direction dir2)
         {
             if (Console.KeyAvailable)
             {
                 var key = Console.ReadKey(intercept: true).Key;
 
-                if (gameClient.playerID == 0) // Первого игрока
+                if (gameClient.playerID == 0) // Управление для первого игрока.
                 {
                     UpdateDirection(ref dir1, key);
-                    gameClient.SendDirection(dir1); // Отправка направления на сервер
+                    gameClient.SendDirection(dir1); // Отправка направления на сервер.
                 }
-                else // Второй игрок
+                else // Управление для второго игрока
                 {
                     UpdateDirection(ref dir2, key);
-                    gameClient.SendDirection(dir2); // Отправка направления на сервер
+                    gameClient.SendDirection(dir2); // Отправка направления на сервер.
                 }
             }
 
-            // Обновление направления противника с учетом текущего состояния
+            // Обновление направления противника.
             if (gameClient.playerID == 0)
             {
-                dir2 = opponentDirection; // Второго игрока
+                dir2 = opponentDirection; // Для второго игрока.
             }
             else
             {
-                dir1 = opponentDirection; // Первого игрока
+                dir1 = opponentDirection; // Для первого игрока.
             }
         }
 
+        /// <summary>
+        /// Обновляет направление змеи в зависимости от нажатой клавиши.
+        /// </summary>
+        /// <param name="direction">Направление змеи (передается по ссылке).</param>
+        /// <param name="key">Нажатая клавиша.</param>
         private void UpdateDirection(ref Direction direction, ConsoleKey key)
         {
             if (key == ConsoleKey.W && direction != Direction.Down) direction = Direction.Up;
@@ -276,6 +315,11 @@ namespace SnakeGame
             else if (key == ConsoleKey.D && direction != Direction.Left) direction = Direction.Right;
         }
 
+        /// <summary>
+        /// Проверяет, не произошло ли столкновение змеи с границами карты или с самой собой.
+        /// </summary>
+        /// <param name="snake">Змея для проверки.</param>
+        /// <returns>True, если произошло столкновение; иначе false.</returns>
         private bool CheckCollision(Snake snake)
         {
             return snake.Head.X == 0 || snake.Head.X == MAP_WIDTH - 1 ||
@@ -283,6 +327,11 @@ namespace SnakeGame
                    snake.Body.Any(b => b.X == snake.Head.X && b.Y == snake.Head.Y);
         }
 
+        /// <summary>
+        /// Отображает счёта обоих игроков на экране.
+        /// </summary>
+        /// <param name="score1">Счёт первого игрока.</param>
+        /// <param name="score2">Счёт второго игрока.</param>
         private void DisplayScores(int score1, int score2)
         {
             SetCursorPosition(2, SCREEN_HEIGHT + 1);
@@ -291,6 +340,11 @@ namespace SnakeGame
             Write($"Игрок 2: {score2}");
         }
 
+        /// <summary>
+        /// Обрабатывает окончание игры и обновляет таблицу рекордов.
+        /// </summary>
+        /// <param name="records">Список рекордов.</param>
+        /// <param name="match">Данные об окончании текущей игры.</param>
         private void HandleGameOver(List<string> records, GameStateData match)
         {
             records.Add($"{match.PlayerName} {match.Score}");
@@ -305,11 +359,15 @@ namespace SnakeGame
             {
                 var keyInfo = ReadKey(true);
                 if (keyInfo.Key == ConsoleKey.Enter)
-                    gameState = GameState.MainMenu;
+                    gameState = GameState.MainMenu; // Возврат в главное меню.
                 break;
             }
         }
 
+        /// <summary>
+        /// Отображает меню паузы и обрабатывает выбор пользователя.
+        /// </summary>
+        /// <param name="selectedItem">Выбранный пункт меню (передается по ссылке).</param>
         private void PauseMenu(ref int selectedItem)
         {
             Clear();
@@ -335,41 +393,53 @@ namespace SnakeGame
             }
             else if (keyInfo.Key == ConsoleKey.Enter)
             {
-                HandlePauseMenuSelection(selectedItem);
+                HandlePauseMenuSelection(selectedItem); // Обработка выбора паузы.
             }
         }
 
+        /// <summary>
+        /// Обрабатывает выбор пункта меню паузы.
+        /// </summary>
+        /// <param name="selectedItem">Выбранный пункт меню.</param>
         private void HandlePauseMenuSelection(int selectedItem)
         {
             if (selectedItem == 2)
             {
-                gameState = GameState.MainMenu;
+                gameState = GameState.MainMenu; // Вернуться в главное меню.
             }
             else if (selectedItem == 0)
             {
-                gameState = GameState.InGame;
+                gameState = GameState.InGame; // Продолжить игру.
             }
             else if (selectedItem == 1)
             {
-                saveLoadManager.SaveData(gameStateData);
+                saveLoadManager.SaveData(gameStateData); // Сохранить игру.
             }
         }
 
+        /// <summary>
+        /// Отрисовывает границы игрового поля.
+        /// </summary>
         private void DrawBoard()
         {
             for (var i = 0; i < MAP_WIDTH; i++)
             {
-                new Pixel(i, 0, BORDER_COLOR).Draw();
-                new Pixel(i, MAP_HEIGHT - 1, BORDER_COLOR).Draw();
+                new Pixel(i, 0, BORDER_COLOR).Draw(); // Верхняя граница
+                new Pixel(i, MAP_HEIGHT - 1, BORDER_COLOR).Draw(); // Нижняя граница
             }
 
             for (var i = 0; i < MAP_HEIGHT; i++)
             {
-                new Pixel(0, i, BORDER_COLOR).Draw();
-                new Pixel(MAP_WIDTH - 1, i, BORDER_COLOR).Draw();
+                new Pixel(0, i, BORDER_COLOR).Draw(); // Левая граница
+                new Pixel(MAP_WIDTH - 1, i, BORDER_COLOR).Draw(); // Правая граница
             }
         }
 
+        /// <summary>
+        /// Генерирует новую еду для змейки.
+        /// </summary>
+        /// <param name="snake">Змея, для которой генерируется еда.</param>
+        /// <returns>Созданный объект Pixel, представляющий еду.</returns>
         private Pixel GenFood(Snake snake)
         {
             Pixel food;
@@ -380,9 +450,13 @@ namespace SnakeGame
             } while (snake.Head.X == food.X && snake.Head.Y == food.Y ||
                      snake.Body.Any(b => b.X == food.X && b.Y == food.Y));
 
-            return food;
+            return food; // Возвращаем сгенерированную еду
         }
 
+        /// <summary>
+        /// Отображает таблицу рекордов.
+        /// </summary>
+        /// <param name="records">Список рекордов для отображения.</param>
         private void ShowRecords(List<string> records)
         {
             Clear();
@@ -392,11 +466,11 @@ namespace SnakeGame
             {
                 if (i >= MAX_RECORDS)
                 {
-                    break;
+                    break; // Выходим из цикла, если достигнуто максимальное количество рекордов
                 }
 
                 string[] record = records[i].Split(' ');
-                WriteLine($"{i + 1}. {record[0]}: {record[1]}");
+                WriteLine($"{i + 1}. {record[0]}: {record[1]}"); // Отображение рекорда
             }
 
             WriteLine("\nНажмите Enter, чтобы вернуться в главное меню.");
